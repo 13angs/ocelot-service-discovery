44a661e69eab409cb3241e9bfc09772d21ae66b4
13angs <13angz.um@gmail.com>
2022-11-19 05:33:49 +0000

    register todo to consul

--- todo/Program.cs
+++ todo/Program.cs
@@ -1,4 +1,10 @@
+using Consul.Client;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var consulConfig = builder.Configuration.GetConsulConfig();
+builder.Services.RegisterConsulServices(consulConfig);
+
 var app = builder.Build();
 
 app.MapGet("/todo/{text}", (string text) => {
